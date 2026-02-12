094e87e3f52a2e0e633656bc1b2ebdeab68f0ab7
Jacob Amaral <jacob_amaral@hotmail.com>
2026-02-12 13:54:20 -0500

    0.9.99 Added buy and hold compare to WF results

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ const Logo: React.FC = () => {
   return (
     <div className="relative h-10 w-40">
       <Image 
-        src="https://assets.co.dev/dc630d7c-c620-481b-9ee9-1959feae8edc/newlogo2ext-94a7d0c.png"
+        src="/stratgen_manual_images/logo.png"
         alt="StratGen automated trading strategy generator logo"
         fill
         style={{ objectFit: 'contain' }}
